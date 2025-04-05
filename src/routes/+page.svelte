2cbbe1e43d494e6ce1a7cfbef33bb051e519990e
Noa <noaminha.student@gmail.com>
2025-04-05 23:55:06 +0200

    finished row responsiveness

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -17,27 +17,27 @@
     <h1 class="font-inter font-semi-bold text-3xl object-bottom">Good Morning {name}!</h1>
 </header>
 
-<main class="bg-[#]">
-    <div class="flex md:flex-row flex-col md:gap-x-4 gap-y-4 md:w-3/4 lg:w-1/2 w-3/4 bg-blue-500 items-center mx-auto">
-        <div class="h-40 md:h-40 gap-x-4 flex justify-between">
-            <button class="min-w-48 bg-[#0358F1] rounded-3xl flex flex-col items-start justify-end p-4 gap-2 hover:opacity-75 transition-opacity duration-50">
+<main>
+    <div class="flex lg:flex-row flex-col lg:gap-x-4 gap-y-4 items-center justify-center mx-auto bg-blue-500">
+        <div class="h-40 gap-4 flex bg-blue-800 w-1/2 max-w-lg lg:justify-end justify-center">
+            <button class="flex-1 min-w-40 max-w-48 bg-[#0358F1] rounded-3xl flex flex-col items-start justify-end p-4 gap-2 hover:opacity-75 transition-opacity duration-50">
                 <ShoppingBasket class="text-white"/>
                 <p class="font-inter text-xl text-white">Checkout</p>
             </button>
 
-            <button class="min-w-48 bg-black rounded-3xl flex flex-col items-start justify-end p-4 gap-2 hover:opacity-75 transition-opacity duration-50">
+            <button class="flex-1 min-w-40 max-w-48 bg-black rounded-3xl flex flex-col items-start justify-end p-4 gap-2 hover:opacity-75 transition-opacity duration-50">
                 <HandPlatter class="text-white"/>
                 <p class="font-inter text-xl text-white">Orders</p>
             </button>
         </div>
 
-        <div class="w-1/2 h-40 flex justify-between gap-x-4">
-            <button class="w-1/2 h-40 bg-black rounded-3xl flex flex-col items-start justify-end p-4 gap-2 hover:opacity-75 transition-opacity duration-50">
+        <div class="h-40 gap-4 flex bg-red-800 w-1/2 max-w-lg lg:justify-start justify-center">
+            <button class="flex-1 min-w-40 max-w-48 bg-black rounded-3xl flex flex-col items-start justify-end p-4 gap-2 hover:opacity-75 transition-opacity duration-50">
                 <Calendar class="text-white" />
                 <p class="font-inter text-xl text-white">Scheduler</p>
             </button>
 
-            <div class="w-1/2 h-40 bg-white rounded-3xl font-inter flex flex-col items-center justify-center text-center">
+            <div class="flex-1 min-w-40 max-w-48 bg-white rounded-3xl font-inter flex flex-col items-center justify-center text-center">
                 <p class="">orders today</p>
                 <p class="font-semi-bold text-3xl">{order_num}</p>
             </div>
