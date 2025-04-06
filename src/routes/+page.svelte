eb522ccea24b06de9cdd3bff6a5ab409c1cc49a9
Noa <noaminha.student@gmail.com>
2025-04-06 08:27:53 +0200

    changed dimensions to static for each size

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -19,25 +19,25 @@
 
 <main class="flex flex-col gap-y-4">
     <div class="flex lg:flex-row flex-col lg:gap-x-4 gap-y-4 items-center justify-center mx-auto">
-        <div class="h-40 gap-4 flex w-1/2 max-w-lg lg:justify-end justify-center">
-            <button class="flex-1 min-w-[170px] max-w-[200px] bg-[#0358F1] rounded-3xl flex flex-col items-start justify-end p-4 gap-2 hover:opacity-75 transition-opacity duration-50">
+        <div class="h-40 gap-4 flex">
+            <button class="w-[170px] lg:w-[200px] bg-[#0358F1] rounded-3xl flex flex-col items-start justify-end p-4 gap-2 hover:opacity-75 transition-opacity duration-50">
                 <ShoppingBasket class="text-white"/>
                 <p class="font-inter text-xl text-white">Checkout</p>
             </button>
 
-            <button class="flex-1 min-w-[170px] max-w-[200px] bg-black rounded-3xl flex flex-col items-start justify-end p-4 gap-2 hover:opacity-75 transition-opacity duration-50">
+            <button class="w-[170px] lg:w-[200px] bg-black rounded-3xl flex flex-col items-start justify-end p-4 gap-2 hover:opacity-75 transition-opacity duration-50">
                 <HandPlatter class="text-white"/>
                 <p class="font-inter text-xl text-white">Orders</p>
             </button>
         </div>
 
-        <div class="h-40 gap-4 flex w-1/2 max-w-lg lg:justify-start justify-center">
-            <button class="flex-1 min-w-[170px] max-w-[200px] bg-black rounded-3xl flex flex-col items-start justify-end p-4 gap-2 hover:opacity-75 transition-opacity duration-50">
+        <div class="h-40 gap-4 flex">
+            <button class="w-[170px] lg:w-[200px] bg-black rounded-3xl flex flex-col items-start justify-end p-4 gap-2 hover:opacity-75 transition-opacity duration-50">
                 <Calendar class="text-white" />
                 <p class="font-inter text-xl text-white">Scheduler</p>
             </button>
 
-            <div class="flex-1 min-w-[170px] max-w-[200px] bg-white rounded-3xl font-inter flex flex-col items-center justify-center text-center">
+            <div class="w-[170px] lg:w-[200px] bg-white rounded-3xl font-inter flex flex-col items-center justify-center text-center">
                 <p class="">orders today</p>
                 <p class="font-semi-bold text-3xl">{order_num}</p>
             </div>
